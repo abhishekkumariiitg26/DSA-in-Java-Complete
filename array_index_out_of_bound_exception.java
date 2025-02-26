4761ff6ed418613474476743515cdf2047980b21
Abhishek Kumar <abhishek.kumar.iiitg.26@gmail.com>
2025-02-26 11:29:07 +0530

    initial commit

---
+++ array_index_out_of_bound_exception.java
@@ -0,0 +1,7 @@
+public class array_index_out_of_bound_exception {
+    public static void main(String[] args){
+        int a[]=new int[5];
+        System.out.println("hello world after exception");
+        //System.out.println(a[5]); //this will give array_index_out_of_bound_exception
+    }
+}
