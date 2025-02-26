8ba9351a4a6f19a481593d6cef8c51c6c6fe0805
Abhishek Kumar <abhishek.kumar.iiitg.26@gmail.com>
2025-02-26 11:54:27 +0530

    initial

--- try_catch_implementation.java
+++ try_catch_implementation.java
@@ -6,11 +6,22 @@ public class try_catch_implementation {
 
         //now using try catch method
         try{
+
+            int b=5/0; // this is a type of arithmetic exception
+            System.out.println(b);
             System.out.println(a[100]);
+
+          //  int b=5/0; //actually this line is not executed as the code go from the upper line to the catch block automatically
         }
-        catch(ArrayIndexOutOfBoundsException e){
+        catch(ArrayIndexOutOfBoundsException | ArithmeticException e){
             System.out.println("trying to print out of bound values of the array");
         }
+        catch(RuntimeException e){
+            System.out.println("runtime exception caught");
+        }
+        // catch(ArithmeticException | RuntimeException e){
+        //     System.out.println("arithmetic exception occured  "+e);
+        // }
 
         System.out.println("ending of code after array_oob_exp");
     }
